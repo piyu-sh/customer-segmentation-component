db38bd05cd9af2b8c090dcdcb8ebeca245a45c97
pi <killerpgupta007@gmail.com>
2016-07-25 20:25:13 +0530

    generation of query done

--- js/app.js
+++ js/app.js
@@ -1,70 +1,156 @@
-$(function () {
+$(function() {
 
 
-    $('.query-region .draggable-btn').each(function () {
+    $('.query-region .draggable-btn').each(function() {
         $(this).draggable({
             containment: $(this).parent().parent(),
             scope: $(this).parent().parent().parent().prop("class"),
-            cancel: false, helper: "original", revert: "invalid",
+            cancel: false,
+            helper: "original",
+            revert: "invalid",
             connectToSortable: ".mdl-grid>div",
         });
     });
 
-    // $(".query-selectors > .draggable-btn ").draggable({
-    //     cancel: false, helper: "clone", revert: "invalid"
-    // });
-
     $(".query-selectors > .draggable-segment ").draggable({
-        cancel: false, helper: "clone", revert: "invalid"
+        cancel: false,
+        helper: "clone",
+        revert: "invalid"
     });
     $(".query-selectors > .draggable-logic ").draggable({
-        cancel: false, helper: "clone", revert: "invalid",
+        cancel: false,
+        helper: "clone",
+        revert: "invalid",
         scope: "draggable-logic"
     });
 
-    // $(".droppable-main-region").droppable({
-    //     accept: ".draggable-logic",
-    //     activeClass: "droppable-highlight"
-    // });
-
-    $(".query-region .and-selector,.query-region .or-selector").each(function () {
+    $(".query-region .and-selector,.query-region .or-selector").each(function() {
         $(this).droppable({
             scope: "draggable-logic",
             activeClass: "droppable-highlight"
         });
     });
 
-    $(".exclude-section").each(function () {
+    $(".exclude-section").each(function() {
         $(this).droppable({
             scope: $(this).parent().parent().prop("class"),
             activeClass: "droppable-highlight",
-            // drop: function (event, ui) {
-            //     // $(ui.draggable).remove();
-            //       var temp=  $(ui.draggable).remove().css({top:"0px",left:"0px"});
-            //     // ui.draggable.appendTo($(this)).fadeIn();
-            //     $(this).append(temp).animate({duration:600});
-            // }
         });
     });
-    $(".include-section").each(function () {
+    $(".include-section").each(function() {
         $(this).droppable({
             scope: $(this).parent().parent().prop("class"),
             activeClass: "droppable-highlight"
         });
     });
 
-    $(".query-region .exclude-section,.query-region .include-section").each(function () {
+    $(".query-region .exclude-section,.query-region .include-section").each(function() {
         $(this).sortable({
             revert: true,
             containment: $(this).parent(),
-            items:">*:not(:first-child)"
+            items: ">*:not(:first-child)"
         });
     });
 
-    $(".generated-query").parent().on( "drop", function( event, ui ) {
-        var queryString=parseDomForQuery($(this));
-    } );
-    function parseDomForQuery(component){
-        
+    $(".generated-query").parent().on("drop", function(event, ui) {
+        var queryString = parseDomForQuery(this);
+        $(".generated-query >div:last-child").html(queryString);
+    });
+
+
+    function parseDomForQuery(component) {
+        var queryString = "";
+        if (isLogicSelector(component)) {
+            queryString = "[";
+        }
+        var compChildren = component.children;
+        if (compChildren.length > 0) {
+            var appendLogic = false;
+            $(compChildren).each(function(index) {
+                if (isSegmentSelector(this)) {
+                    if (appendLogic) {
+                        queryString += " " + selectorType(component) + " "; //check for last occurence and add "'"
+                    }
+                    appendLogic = true;
+                    queryString += "'" + selectorType(this) + "=";
+                    var includedList = includedOptions(this);
+                    var excludedList = excludedOptions(this);
+                    if (includedList.length == 0) {
+                        queryString += "none";
+                    } else if (excludedList.length == 0) {
+                        queryString += "all";
+                    } else if (includedList.length > excludedList.length) {
+                        queryString += "all except " + excludedList.join(",");
+                    } else {
+                        queryString += includedList.join(",");
+                    }
+                    queryString += "'";
+                } else {
+                    if (isLogicSelector(this)) {
+                        if (appendLogic) {
+                            queryString += " " + selectorType(component) + " "; //check for last occurence and add "'"
+                        }
+                        appendLogic = true;
+                    }
+                    queryString += parseDomForQuery(this);
+                }
+            });
+        }
+        if (isLogicSelector(component)) {
+            queryString += "]";
+        }
+        return queryString;
     }
+
+    function isLogicSelector(component) {
+        if ($(component).hasClass("and-selector") || $(component).hasClass("or-selector")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function selectorType(component) {
+        var type;
+        if ($(component).hasClass("and-selector")) {
+            type = "and";
+        } else if ($(component).hasClass("or-selector")) {
+            type = "or";
+        } else if ($(component).hasClass("location-selector")) {
+            type = "location";
+        } else if ($(component).hasClass("browser-selector")) {
+            type = "browser";
+        } else if ($(component).hasClass("os-selector")) {
+            type = "os";
+        } else if ($(component).hasClass("day-selector")) {
+            type = "day";
+        } else if ($(component).hasClass("visitor-type-selector")) {
+            type = "visitor-type";
+        } else if ($(component).hasClass("mobile-selector")) {
+            type = "mobile";
+        }
+        return type;
+    }
+
+    function isSegmentSelector(component) {
+        return !isLogicSelector(component) && typeof selectorType(component) != 'undefined';
+    }
+
+    function includedOptions(component) {
+        var optionsArray = [];
+        $(component).find(".include-section button:not(*[class*='ui-sortable-placeholder'])").each(function() {
+            optionsArray.push($(this).html());
+        });
+        return optionsArray;
+    }
+
+    function excludedOptions(component) {
+        var optionsArray = [];
+        $(component).find(".exclude-section button:not(*[class*='ui-sortable-placeholder'])").each(function() {
+            optionsArray.push($(this).html());
+        });
+        return optionsArray;
+    }
+
+    
 });
